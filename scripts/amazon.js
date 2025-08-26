b33b5dcfaef69e91e6f331fdcfe49fa1ecc0972a
Dayved005 <davechilex005@gmail.com>
2025-08-26 21:18:15 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -86,8 +86,5 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
-      
-      console.log(cartQuantity);
-      console.log(cart);
     });
   });
