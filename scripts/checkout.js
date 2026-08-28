33edaf35d045d2871a9179f7fabf63daa046aede
Dayved005 <davechilex005@gmail.com>
2026-08-28 06:40:24 +0100

    Used Promise.all to run loadProducts.Fetch and loadCartFetch at the same time & made the inner function async and use await to wait for loadProductFetch to finish in orderSummaryTest.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,8 @@
-import { renderOrderSummary } from './checkout/orderSummary.js';
-import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { renderCheckOutHeader } from './checkout/checkoutHeader.js';
-import { loadProducts, loadProductsFetch } from '../data/products.js';
-import { loadCart, loadCartFetch } from '../data/cart.js';
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { renderCheckOutHeader } from "./checkout/checkoutHeader.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
@@ -10,17 +10,17 @@ async function loadPage() {
   try {
     // throw 'error1';
 
-    await loadProductsFetch();
-
-    await loadCartFetch();
-
-  } catch (error) { 
-    console.log('Unexpected error. Please try again later.');
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+    
+  } catch (error) {
+    console.log("Unexpected error. Please try again later.");
   }
 
-
   renderOrderSummary();
-  renderPaymentSummary();  
+  renderPaymentSummary();
   renderCheckOutHeader();
 }
 loadPage();
